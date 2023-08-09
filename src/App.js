d4942ddce50b1dbf7ceab135fe7cc10d392519ce
Robert Nickson <106345947+RN255@users.noreply.github.com>
2023-08-09 22:32:31 +0100

    reading from modules

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { arrayOfLessons } from "./modules/lessonText";
+import MainConvo from "./components/MainConvo";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
       <Home></Home>
+      <MainConvo LessonText={arrayOfLessons[0]}></MainConvo>
     </>
   );
 }
